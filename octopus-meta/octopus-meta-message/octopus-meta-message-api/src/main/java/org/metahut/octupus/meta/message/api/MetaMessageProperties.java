5016aad904b278318b1a44c79a6b865dafed5517
zhuangchong <zhuangchong6@163.com>
2022-02-17 17:34:16 +0800

    add message plugin in the meta server

---
+++ octopus-meta/octopus-meta-message/octopus-meta-message-api/src/main/java/org/metahut/octupus/meta/message/api/MetaMessageProperties.java
@@ -0,0 +1,9 @@
+package org.metahut.octupus.meta.message.api;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(
+        prefix = "meta.message"
+)
+public class MetaMessageProperties {
+}
