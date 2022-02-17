583baf8d2768985d9363b8e28d7a0ae5dd98ad8b
xu1 xuyang <xu1.xuyang@zhaopin.com.cn>
2022-02-17 17:15:19 +0800

    update

---
+++ octopus-meta/octopus-meta-parser/src/test/java/org/metahut/octopus/meta/parser/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * MetaData
+ * //https://wenku.baidu.com/view/014a96bbccbff121dc36838e.html
+ * //https://atlas.apache.org/#/TypeSystem
+ *
+ * //JOT
+ * //javaCompile
+ * //https://seanwangjs.github.io/2018/03/13/java-runtime-compile.html
+ * //janino
+ * //http://janino-compiler.github.io/janino/
+ * @author XuYang
+ * Create at 2022/2/17
+ * @description
+ */
+package org.metahut.octopus.meta.parser;
