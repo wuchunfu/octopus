7959940e427ed442a2e25db8e12399a6d97f5cd0
Moouna <a171582490@163.com>
2022-02-25 14:01:33 +0800

    update

--- octopus-meta/octopus-meta-parser/src/test/java/org/metahut/octopus/meta/parser/utils/OQLUtilTest.java
+++ octopus-meta/octopus-meta-parser/src/test/java/org/metahut/octopus/meta/parser/utils/OQLUtilTest.java
@@ -16,8 +16,10 @@ public class OQLUtilTest {
 
     /**
      * OQL javaVM query language
-     * objectbox-java a database for javaObject
-     * alibaba COLA
+     * objectbox-java a database for javaObject (X)
+     * alibaba COLA (X)
+     * cglib
+     *
      *
      */
 
