95f657d4242ac58920eb2af9970e6b55b3d7cc6b
xiaoliang.wang <68142195+wanderEverywhere@users.noreply.github.com>
2022-05-07 11:42:59 +0800

    add ruleTemplate response (#53)

---
+++ octopus-api/src/main/java/org/metahut/octopus/api/dto/RuleTemplateResponseDTO.java
@@ -0,0 +1,54 @@
+package org.metahut.octopus.api.dto;
+
+import org.metahut.octopus.common.enums.SubjectCategoryEnum;
+import org.metahut.octopus.common.enums.ThresholdUnit;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@ApiModel(value = "ruleTemplate response")
+public class RuleTemplateResponseDTO {
+    @ApiModelProperty(value = "id")
+    private Integer id;
+
+    @ApiModelProperty(value = "code")
+    private Long code;
+
+    @ApiModelProperty(value = "templateName")
+    private String templateName;
+
+    @ApiModelProperty(value = "metricsName")
+    private String metricsName;
+
+    @ApiModelProperty(value = "subjectCategoryEnum")
+    private SubjectCategoryEnum subjectCategoryEnum;
+
+    @ApiModelProperty(value = "verificationType")
+    private String verificationType;
+
+    @ApiModelProperty(value = "verificationWay")
+    private String verificationWay;
+
+    @ApiModelProperty(value = "thresholdUnit")
+    private ThresholdUnit thresholdUnit;
+
+    @ApiModelProperty(value = "thresholds")
+    private List<BigDecimal> thresholds;
+
+    @ApiModelProperty(value = "creator")
+    private Integer creator;
+
+    @ApiModelProperty(value = "operator")
+    private Integer operator;
+
+    @ApiModelProperty(value = "createTime")
+    private Date createTime;
+
+
+    @ApiModelProperty(value = "updateTime")
+    private Date updateTime;
+}
--- octopus-server/src/main/java/org/metahut/octopus/server/controller/RuleTemplateControllerImpl.java
+++ octopus-server/src/main/java/org/metahut/octopus/server/controller/RuleTemplateControllerImpl.java
@@ -4,35 +4,33 @@ import org.metahut.octopus.api.controller.RuleTemplateController;
 import org.metahut.octopus.api.dto.PageRequestDTO;
 import org.metahut.octopus.api.dto.ResultEntity;
 import org.metahut.octopus.api.dto.RuleTemplateRequestDTO;
+import org.metahut.octopus.api.dto.RuleTemplateResponseDTO;
+import org.metahut.octopus.api.dto.response.PageResponseDTO;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RuleTemplateControllerImpl implements RuleTemplateController {
 
-    private final RuleTemplateController ruleTemplateController;
-
-    public RuleTemplateControllerImpl(RuleTemplateController ruleTemplateController) {
-        this.ruleTemplateController = ruleTemplateController;
+    @Override
+    public ResultEntity<PageResponseDTO<RuleTemplateResponseDTO>> queryRuleTemplatePage(PageRequestDTO<RuleTemplateRequestDTO> ruleTemplateRequest) {
+        return ResultEntity.success();
     }
 
     @Override
-    public ResultEntity queryRuleTemplatePage(PageRequestDTO<RuleTemplateRequestDTO> ruleTemplateRequest) {
-        return ResultEntity.success(ruleTemplateController.queryRuleTemplatePage(ruleTemplateRequest));
+    public ResultEntity<Boolean> deleteRuleTemplate(Integer id) {
+        return ResultEntity.success();
     }
 
     @Override
-    public ResultEntity deleteRuleTemplate(Integer id) {
-        return ResultEntity.success(ruleTemplateController.deleteRuleTemplate(id));
+    public ResultEntity<RuleTemplateResponseDTO> createRuleTemplate(RuleTemplateRequestDTO ruleTemplateRequest) {
+        return ResultEntity.success();
     }
 
     @Override
-    public ResultEntity createRuleTemplate(RuleTemplateRequestDTO ruleTemplateRequest) {
-        return ResultEntity.success(ruleTemplateController.createRuleTemplate(ruleTemplateRequest));
-    }
-
-    @Override
-    public ResultEntity updateRuleTemplate(RuleTemplateRequestDTO ruleTemplateRequest) {
-        return ResultEntity.success(ruleTemplateController.updateRuleTemplate(ruleTemplateRequest));
+    public ResultEntity<RuleTemplateResponseDTO> updateRuleTemplate(RuleTemplateRequestDTO ruleTemplateRequest) {
+        return ResultEntity.success();
     }
 }
