1e31c6f808cf4f4f48060b065184df7351ad91be
xiaoliang.wang <15210284595@163.com>
2022-06-02 17:06:12 +0800

    update

--- octopus-server/src/main/java/org/metahut/octopus/server/service/SampleInstanceService.java
+++
@@ -1,9 +0,0 @@
-package org.metahut.octopus.server.service;
-
-import org.metahut.octopus.api.dto.SampleInstanceCreateOrUpdateRequestDTO;
-import org.metahut.octopus.dao.entity.SampleInstance;
-
-public interface SampleInstanceService {
-
-    SampleInstance checkSample(SampleInstanceCreateOrUpdateRequestDTO sample);
-}
